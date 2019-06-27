741306a14da56ecfc896bc7fdd608cc8a32154fa
fabiovpcaumo <fabiovpcaumo@gmail.com>
2019-06-26 21:47:19 -0300

    Telas dos menus de opções abrindo no menu

--- Prova02-LP-B/Form1.cs
+++ Prova02-LP-B/Form1.cs
@@ -19,22 +19,30 @@ namespace Prova02_LP_B
 
         private void BtnOption1_Click(object sender, EventArgs e)
         {
-
+            FormOption1 formOption1 = new FormOption1();
+            formOption1.Show();
+            this.Hide();
         }
 
         private void BtnOption2_Click(object sender, EventArgs e)
         {
-
+            FormOption2 formOption2 = new FormOption2();
+            formOption2.Show();
+            this.Hide();
         }
 
         private void BtnOption3_Click(object sender, EventArgs e)
         {
-
+            FormOption3 formOption3 = new FormOption3();
+            formOption3.Show();
+            this.Hide();
         }
 
         private void BtnOption4_Click(object sender, EventArgs e)
         {
-
+            FormOption4 formOption4 = new FormOption4();
+            formOption4.Show();
+            this.Hide();
         }
 
         private void BtnOption5_Click(object sender, EventArgs e)
